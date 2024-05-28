1e11d22001b53d7482c4438076451a492f4d21fc
LeandroSousaDev <leandro_s.sousa@hotmail.com>
2024-05-28 17:01:14 -0300

    atividades feitas

--- src/SupplierExample.java
+++ src/SupplierExample.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class SupplierExample {
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class SupplierExample {
+    public static void main(String[] args) {
+        Supplier<String> saudacao = () -> "Olá, seja bem-vindo(a)!";
+
+        List<String> listaSaudacoes = Stream.generate(saudacao).limit(5).toList();
+        List<String> listaDeSaudacoes = Stream.generate(() -> "Ola, Bem vindo").limit(3).toList();
+
+        listaSaudacoes.forEach(System.out::println);
+        listaDeSaudacoes.forEach(System.out::println);
+    }
 }
